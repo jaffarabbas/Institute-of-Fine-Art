67cc59ff8dbf9e3849815d496c37e19c379a94c4
Jaffar Abbas <gamaportal8@gmail.com>
2021-11-21 20:06:38 +0500

    21/11/2021 20:06:36

--- InstituteOfFineArt/Models/FineLogin.Designer.cs
+++ InstituteOfFineArt/Models/FineLogin.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'J:\c#Work\Institute-of-Fine-Art\InstituteOfFineArt\Models\FineLogin.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Github\Institute-of-Fine-Art\InstituteOfFineArt\Models\FineLogin.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
